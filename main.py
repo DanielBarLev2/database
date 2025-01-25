162fe4e8e5220c7d3866ba4f9c17a0222ad983be
DanielBarLev <danielbarlev00@gmail.com>
2025-01-25 22:17:49 +0200

    Refactor database script and update related files
    Refactor `create_db_script.py` to streamline operations, add database connection logic, and include placeholders for schema creation and data loading.

--- main.py
+++ main.py
@@ -5,38 +5,4 @@ from src import create_db_script
 
 
 if __name__ == "__main__":
-    create_db_script.download_and_extract_dataset()
-
-
-# try:
-#     # create an SSH tunnel
-#     print("Establishing SSH tunnel...")
-#     with SSHTunnelForwarder(ssh_address_or_host=cfg.SSH_CONFIG['ssh_address_or_host'],
-#                             ssh_username=cfg.SSH_CONFIG['ssh_username'],
-#                             ssh_password=None,
-#                             remote_bind_address=cfg.SSH_CONFIG['remote_bind_address'],
-#                             local_bind_address=cfg.SSH_CONFIG['local_bind_address']) as tunnel:
-#
-#         print(f"Tunnel established: Local port {tunnel.local_bind_port}")
-#
-#         try:
-#             print("Connecting to MySQL server...")
-#             connection = mysql.connector.connect(
-#                 host=cfg.DB_CONFIG['host'],
-#                 port=tunnel.local_bind_port,
-#                 user=cfg.DB_CONFIG['user'],
-#                 password=cfg.DB_CONFIG['password'],
-#                 database=cfg.DB_CONFIG['database'],
-#                 connection_timeout=60
-#             )
-#             print("Connected to MySQL server")
-#         except mysql.connector.Error as err:
-#             print(f"MySQL connection error: {err}")
-#         finally:
-#             if 'connection' in locals() and connection.is_connected():
-#                 connection.close()
-#                 print("MySQL connection closed")
-#
-#
-# except Exception as e:
-#     print(f"An error occurred: {e}")
+    create_db_script.main()
--- src/create_db_script.py
+++ src/create_db_script.py
@@ -1,29 +1,20 @@
-# Contains code responsible for creating the database.
+"""Contains code responsible for creating the database"""
 
 import os
-import zipfile
 import kaggle
-import pandas as pd
-import json
 import mysql.connector
-from kaggle.api.kaggle_api_extended import KaggleApi
+from sshtunnel import SSHTunnelForwarder
+
 from config import config as cfg
 
 
 def download_and_extract_dataset():
     """
-    Downloads and extracts a dataset from Kaggle.
+    Creates a directory named data and downloads and unzip the dataset from kaggle.
 
-    This function performs the following steps:
-       1. Creates a directory at the configured download path if it does not
-          exist.
-       2. Authenticates the user with the Kaggle API.
-       3. Downloads and extracts the specified dataset into the created
-          directory.
-
-    :raises OSError: Raised if the target directory already exists.
-    :raises kaggle.rest.ApiException: Raised if an error occurs when interacting
-                                      with the Kaggle API.
+    Expected files:
+        data/tmdb_5000_credits.csv
+        data/tmdb_5000_movies.csv
     """
     try:
         os.makedirs(cfg.DOWNLOAD_PATH, exist_ok=False)
@@ -39,9 +30,56 @@ def download_and_extract_dataset():
     except OSError:
         print(f"dataset is available at {cfg.DOWNLOAD_PATH}")
 
+
+def create_database_schema(cursor):
+    """
+        @todo: Create the database schema.
+    """
+    print("Creating database schema...")
+
+
+def load_data_to_database(cursor, connection):
+    """
+        @todo: Load the dataset into the database.
+    """
+
+
+
 def main():
+
     download_and_extract_dataset()
 
+    print("Establishing SSH tunnel...")
+    with SSHTunnelForwarder(ssh_address_or_host=cfg.SSH_CONFIG['ssh_address_or_host'],
+                            ssh_username=cfg.SSH_CONFIG['ssh_username'],
+                            ssh_password=None,
+                            remote_bind_address=cfg.SSH_CONFIG['remote_bind_address'],
+                            local_bind_address=cfg.SSH_CONFIG['local_bind_address']) as tunnel:
 
-if __name__ == "__main__":
-    main()
+        print(f"Tunnel established: Local port {tunnel.local_bind_port}")
+
+        try:
+            print("Connecting to MySQL server...")
+            connection = mysql.connector.connect(
+                host=cfg.DB_CONFIG['host'],
+                port=tunnel.local_bind_port,
+                user=cfg.DB_CONFIG['user'],
+                password=cfg.DB_CONFIG['password'],
+                database=cfg.DB_CONFIG['database'],
+                connection_timeout=60)
+
+            print("Connected to MySQL server")
+
+            cursor = connection.cursor()
+            create_database_schema(cursor)
+            print()
+            load_data_to_database(cursor, connection)
+
+        except mysql.connector.Error as err:
+            print(f"MySQL connection error: {err}")
+
+        finally:
+            # finally close the connection
+            if 'connection' in locals() and connection.is_connected():
+                connection.close()
+                print("MySQL connection closed")
