e41068a63e6f3109607a5ea431582c64b43827d9
DanielBarLev <danielbarlev00@gmail.com>
2025-01-25 21:34:17 +0200

    Add dataset download functionality and initial configurations
    Added a script to download and extract datasets from Kaggle, including configuration settings for SSH and database connections. Updated `requirements.txt` with necessary dependencies and added stubs for database interaction in `main.py`.

---
+++ config/config.py
@@ -0,0 +1,21 @@
+import os
+
+
+DATASET_NAME = "tmdb/tmdb-movie-metadata"
+DOWNLOAD_PATH = os.path.join(os.getcwd(), "data")
+
+SSH_CONFIG = {
+    "ssh_address_or_host": ("nova.cs.tau.ac.il", 22),
+    "ssh_username": "danielbarlev",
+    "ssh_password": "<password>",
+    "remote_bind_address": ("mysqlsrv1.cs.tau.ac.il", 3306),
+    "local_bind_address":( "127.0.0.1", 3305)
+}
+
+DB_CONFIG = {
+    "host": "127.0.0.1",
+    "port": 3305,
+    "user": "annap",
+    "password": "passaword",
+    "database": "annap"
+}
--- src/create_db_script.py
+++ src/create_db_script.py
@@ -1,1 +1,47 @@
 # Contains code responsible for creating the database.
+
+import os
+import zipfile
+import kaggle
+import pandas as pd
+import json
+import mysql.connector
+from kaggle.api.kaggle_api_extended import KaggleApi
+from config import config as cfg
+
+
+def download_and_extract_dataset():
+    """
+    Downloads and extracts a dataset from Kaggle.
+
+    This function performs the following steps:
+       1. Creates a directory at the configured download path if it does not
+          exist.
+       2. Authenticates the user with the Kaggle API.
+       3. Downloads and extracts the specified dataset into the created
+          directory.
+
+    :raises OSError: Raised if the target directory already exists.
+    :raises kaggle.rest.ApiException: Raised if an error occurs when interacting
+                                      with the Kaggle API.
+    """
+    try:
+        os.makedirs(cfg.DOWNLOAD_PATH, exist_ok=False)
+
+        try:
+            kaggle.api.authenticate()
+            kaggle.api.dataset_download_files(cfg.DATASET_NAME, path=cfg.DOWNLOAD_PATH, unzip=True)
+        except:
+            raise "kaggle.json must be placed in ~/.kaggle (usually at users/user)"
+
+        print(f"dataset downloaded and unzipped and saved to {cfg.DOWNLOAD_PATH}.")
+
+    except OSError:
+        print(f"dataset is available at {cfg.DOWNLOAD_PATH}")
+
+def main():
+    download_and_extract_dataset()
+
+
+if __name__ == "__main__":
+    main()
