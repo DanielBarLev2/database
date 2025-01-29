3254a2675d4167c6a90c1e4598eb29f9d6cadb65
DanielBarLev <danielbarlev00@gmail.com>
2025-01-29 16:57:55 +0200

    Redesign db: movies and genres are populated

--- src/create_db_script.py
+++ src/create_db_script.py
@@ -1,13 +1,12 @@
 """Contains code responsible for creating the database"""
 
 import os
-import json
-
 import pandas as pd
 import mysql.connector
 from sshtunnel import SSHTunnelForwarder
 
 from config import config as cfg
+from utils.load_data_utils import get_table_columns, insert_data, process_json_column
 
 
 def download_and_extract_dataset():
@@ -47,93 +46,47 @@ def create_database_schema(cursor):
     """
     print("creating database schema...")
 
-    create_table_queries = {
-        "create_movies_table": """
-        CREATE TABLE IF NOT EXISTS Movies (
+    tables = {
+        "movies":
+        """ CREATE TABLE IF NOT EXISTS Movies (
             movie_id INT PRIMARY KEY,
-            title VARCHAR(255),
             budget BIGINT,
             original_language VARCHAR(10),
             original_title VARCHAR(255),
+            overview VARCHAR(1023),
+            popularity FLOAT,
             release_date DATE,
             revenue BIGINT,
             runtime INT,
-            status VARCHAR(50),
+            status VARCHAR(15),
+            title VARCHAR(255),
             vote_average FLOAT,
-            vote_count INT,
-            popularity FLOAT
-            );
-        """,
-        "create_genres_table": """
-        CREATE TABLE IF NOT EXISTS Genres (
-            genre_id INT PRIMARY KEY,
-            genre_name VARCHAR(100)
-            );
-        """,
-        "create_movie_genres_table": """
-        CREATE TABLE IF NOT EXISTS Movie_Genres (
-            movie_id INT,
-            genre_id INT,
-            PRIMARY KEY (movie_id, genre_id),
-            FOREIGN KEY (movie_id) REFERENCES Movies(movie_id),
-            FOREIGN KEY (genre_id) REFERENCES Genres(genre_id)
-            );
-        """,
-        "create_production_companies_table": """
-        CREATE TABLE IF NOT EXISTS Production_Companies (
-            company_id INT PRIMARY KEY,
-            company_name VARCHAR(255)
-            );
-        """,
-        "create_movie_companies_table": """
-        CREATE TABLE IF NOT EXISTS Movie_Companies (
-            movie_id INT,
-            company_id INT,
-            PRIMARY KEY (movie_id, company_id),
-            FOREIGN KEY (movie_id) REFERENCES Movies(movie_id),
-            FOREIGN KEY (company_id) REFERENCES Production_Companies(company_id)
-            );
-        """,
-        "create_credits_table": """
-        CREATE TABLE IF NOT EXISTS Credits (
-            credit_id INT PRIMARY KEY,
-            movie_id INT,
-            person_id INT,
-            role ENUM('actor', 'crew'),
-            `character` VARCHAR(255),
-            job VARCHAR(100),
-            FOREIGN KEY (movie_id) REFERENCES Movies(movie_id)
-            );
-        """,
-        "create_keywords_table": """
-        CREATE TABLE IF NOT EXISTS Keywords (
-            keyword_id INT PRIMARY KEY,
-            keyword_name VARCHAR(255)
-            );
-        """,
-        "create_movie_keywords_table": """
-        CREATE TABLE IF NOT EXISTS Movie_Keywords (
-            movie_id INT,
-            keyword_id INT,
-            PRIMARY KEY (movie_id, keyword_id),
-            FOREIGN KEY (movie_id) REFERENCES Movies(movie_id),
-            FOREIGN KEY (keyword_id) REFERENCES Keywords(keyword_id)
-            );
-        """}
+            vote_count INT);""",
+        "genres": """
+            CREATE TABLE IF NOT EXISTS Genres (
+                genre_id INT PRIMARY KEY,
+                genre_name VARCHAR(100)
+                );""",
+        "movies_genres": """
+            CREATE TABLE IF NOT EXISTS Movie_Genres (
+                movie_id INT,
+                genre_id INT,
+                PRIMARY KEY (movie_id, genre_id),
+                FOREIGN KEY (movie_id) REFERENCES Movies(movie_id),
+                FOREIGN KEY (genre_id) REFERENCES Genres(genre_id));"""}
 
-    for table, query in create_table_queries.items():
+    for table, query in tables.items():
         cursor.execute(query)
+        print(f"+ {table} table was created")
 
-        table_name = table.split('_')[1]
-        print(f"* {table_name} table was created")
-
-    print("database schema created successfully!")
+    print("database schema created successfully.")
 
 
 def drop_all_tables(cursor, connection):
     """
         Drops all tables in the current database.
     """
+    print("deleting database schema...")
     try:
         cursor.execute("SHOW TABLES;")
         tables = cursor.fetchall()
@@ -147,13 +100,13 @@ def drop_all_tables(cursor, connection):
 
         for (table_name,) in tables:
             cursor.execute(f"DROP TABLE {table_name};")
-            print(f"* {table_name} table was dropped.")
+            print(f"- {table_name} table was dropped.")
 
         # re-enable foreign key checks
         cursor.execute("SET FOREIGN_KEY_CHECKS = 1;")
 
         connection.commit()
-        print("All tables dropped successfully.")
+        print("database schema was dropped successfully.")
 
     except Exception as e:
         print(f"Error while dropping tables: {e}")
@@ -165,81 +118,24 @@ def load_data_to_database(cursor, connection):
         Load the dataset into the database.
         This is only done if the tables are empty, since its initialize the database.
     """
+    print("loading database schema...")
 
     # load datasets
     movies_data = pd.read_csv(cfg.MOVIE_DATA_PATH)
     credits_data = pd.read_csv(cfg.CREDITS_DATA_PATH)
 
-    def get_table_columns(_cursor, _table_name):
-        """
-        @:return: the column names of a given table.
-        """
-        _cursor.execute(f"DESCRIBE {_table_name}")
-        return [_row[0] for _row in _cursor.fetchall()]
+    movies_df = movies_data.copy()
+    movies_df.rename(columns={'id': 'movie_id'}, inplace=True) # dataset and sql table first column name mismatch.
+    movies_df = movies_df[get_table_columns(cursor=cursor, table_name="Movies")]
+    insert_data(cursor=cursor, table_name="Movies", data=movies_df)
 
-    def handle_missing_values(_data):
-        """
-            Handles missing values in the dataset by replacing them with defaults.
-            - NaN Numeric columns are replaced with 0
-            - NaN String columns are replaced with an empty string
-            - Replace invalid or empty dates with NaT (Not a Time)
-        """
-        for column in _data.columns:
-            if column == 'release_date':
-
-                _data[column] = pd.to_datetime(_data[column], errors='coerce')
-                _data[column] = _data[column].where(_data[column].notnull(), None)
-
-            elif _data[column].dtype == 'object':
-                _data[column].fillna('', inplace=True)
-
-            else:
-                _data[column].fillna(0, inplace=True)
-
-        return _data
-
-    def insert_data(_cursor, _table_name, _data):
-        """
-        Inserts data into a specified table.
-        """
-        # skips if already loaded
-        cursor.execute(f"SELECT COUNT(*) FROM {table_name};")
-        row_count = cursor.fetchone()[0]
-        if row_count > 0:
-            print(f"{table_name} table was already loaded. skip insertion")
-            return
-
-        _columns = get_table_columns(_cursor, _table_name)
-
-        # change name from csv column -> sql table column
-        column_mapping = {col: col for col in _columns}
-        if "id" in _data.columns and "movie_id" in _columns:
-            column_mapping["id"] = "movie_id"
-
-        _data = _data.rename(columns=column_mapping)
-        _data = _data[_columns]
-
-        # handle missing values
-        _data = handle_missing_values(_data)
-
-        # Generate placeholders and query
-        _placeholders = ", ".join(["%s"] * len(_columns))
-        insert_query = f"""
-            INSERT INTO {_table_name} ({', '.join(_columns)}) 
-            VALUES ({_placeholders})
-        """
-        for _, _row in _data.iterrows():
-            _cursor.execute(insert_query, tuple(_row.values))
-
-        print(f"* {_table_name} was populated.")
-
-    # populate Movies table
-    table_name ="Movies"
-    insert_data(cursor, table_name, movies_data)
-
+    genre_df = process_json_column(df=movies_data, column_name="genres")
+    genre_df.columns = get_table_columns(cursor=cursor, table_name="Genres")
+    genre_df = genre_df.drop_duplicates(subset=['genre_name'])
+    insert_data(cursor=cursor, table_name="Genres", data=genre_df)
 
     connection.commit()
-    print("Data loading completed!")
+    print("All data loading completed!")
 
 def main():
 
@@ -269,7 +165,7 @@ def main():
 
             cursor = connection.cursor()
 
-            drop_all_tables(cursor, connection)
+            # drop_all_tables(cursor, connection)
             create_database_schema(cursor)
             load_data_to_database(cursor, connection)
 
